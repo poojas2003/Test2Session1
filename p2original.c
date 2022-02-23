7409428db9d530a4f79d7f885493cf2398acf49a
Pooja <96231234+poojas2003@users.noreply.github.com>
2022-02-23 05:00:18 +0000

    area of triangle

---
+++ p2original.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+{
+  void input_triangle(float*x1,float*y1,float*x2,float*y2,float*x3,float*y3)
+{
+  printf("enter the values of x1,y1,x2,y2,x3,y3\n");
+  scanf("%f%f%f%f%f%f",x1,y1,x2,y2,x3,y3);
+}
+int is_triangle(float x1,float y1,float x2,float y2,float x3,float y3)
+  {
+    
+  }
