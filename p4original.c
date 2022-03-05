bc667e59d0a2ebf97e0225c590351882b5a4a358
Pooja <96231234+poojas2003@users.noreply.github.com>
2022-03-05 07:02:28 +0000

    p4original.c

---
+++ p4original.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int input()
+{
+  int n;
+  printf("enter the value of n\n");
+  scanf("%d",&n);
+  return n;
+}
+int find_fibo(int n)
+{  
+   if(n==0 || n==1)
+     return n;
+   else
+     return find_fibo(n-1)+find_fibo(n-2);
+
+} 
+void output(int n, int fibo)
+{
+  printf("%d is the nth number of %d fibonaacci series\n",fibo,n);
+  
+}
+int main()
+{
+  int n,fibo;
+  n=input();
+  fibo=find_fibo(n);
+  output(n,fibo);
+  return 0;
+}
